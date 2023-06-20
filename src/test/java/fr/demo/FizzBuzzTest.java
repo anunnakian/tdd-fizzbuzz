56f76c58dc5c8d4d216b76d79a02c74331f1622c
Mohamed <mohamed.aitabderrahman@gmail.com>
2023-06-20 14:36:27 +0200

    Return Fizz when the number is divisible by 3

--- src/test/java/fr/demo/FizzBuzzTest.java
+++ src/test/java/fr/demo/FizzBuzzTest.java
@@ -6,8 +6,15 @@ import org.junit.jupiter.api.Test;
 class FizzBuzzTest {
 
     @Test
-    void should_return_Fizz() {
+    void should_return_Fizz_when_3() {
         String result = FizzBuzz.evaluate(3);
+
+        Assertions.assertEquals("Fizz", result);
+    }
+
+    @Test
+    void should_return_Fizz_when_divisible_by_3() {
+        String result = FizzBuzz.evaluate(6);
 
         Assertions.assertEquals("Fizz", result);
     }
